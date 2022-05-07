0b5c397cdaff2a3d918af76ed8def90af93a8f92
WarmMilkCodes <dylncox@gmail.com>
2022-05-06 22:55:47 -0500

    Put scraper into bot command to auto update results

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ import config
 from bs4 import BeautifulSoup
 import requests
 import html5lib
+import time
 #from scraper import CovidCases
 
 #import scraper
@@ -13,42 +14,36 @@ import html5lib
 # Bot Variables
 bot = commands.Bot(command_prefix = '!', case_insensitive=True)
 
-# Bot Online Confirmation
-def scrape():
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} is online.')
+
+
+# Bot commands
+
+@bot.slash_command(guild_ids=[879461344322138173], description="To see if bot is responding to commands.")
+async def checkworking(ctx):
+    await ctx.respond("I am here.")
+    
+    
+@bot.slash_command(guild_ids=[879461344322138173], description="Worldwide COVID cases")
+async def covidcounter(ctx):
     URL = "https://www.worldometers.info/coronavirus/"
     r = requests.get(URL)
     soup = BeautifulSoup(r.content, 'html5lib')
     covidCases = soup.find('div', class_="maincounter-number")
     counter = covidCases.text.strip()
     print(counter)
-
-'''
-URL = "https://www.worldometers.info/coronavirus/"
-r = requests.get(URL)
-soup = BeautifulSoup(r.content, 'html5lib')
-#print(soup.prettify())
-covidCases = soup.find('div', class_="maincounter-number")
-counter = covidCases.text.strip()
-print(counter)
-'''
-
-@bot.event
-async def on_ready():
-    print(f'{bot.user} is online.')
-
-# Bot command for returning Covid cases
-
-@bot.slash_command(guild_ids=[879461344322138173], description="To see if bot is responding to commands.")
-async def checkworking(ctx):
-    await ctx.respond("I am here.")
+    await ctx.respond('The current number of COVID-19 cases to date is %s.' % counter)
     
-@bot.slash_command(guild_ids=[879461344322138173], description="Worldwide COVID cases")
-async def covidcounter(ctx):
-    await ctx.respond("There are currently %s COVID-19 cases worldwide." % scrape())
     
 @bot.slash_command(guild_ids=[879461344322138173], description="Information on COVID-19 Pandemic")
 async def covidinfo(ctx):
     await ctx.respond("For more information on COVID-19 visit: https://www.cdc.gov/coronavirus/2019-ncov/index.html")
-    
+
+
+
     
 bot.run = bot.run(config.BOT_TOKEN)
+
