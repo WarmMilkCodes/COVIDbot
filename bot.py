17af0d47b3ff7d8d70312866373d8014c1424b81
WarmMilkCodes <dylncox@gmail.com>
2022-05-07 00:11:24 -0500

    Working on Vaccine Location

--- bot.py
+++ bot.py
@@ -1,10 +1,11 @@
 import discord
 from discord.ext import commands
 from discord.commands import Option
+from numpy import real
 import config
 from bs4 import BeautifulSoup
 import requests
-import html5lib
+from uszipcode import SearchEngine
 
 # Bot Variables
 bot = commands.Bot(command_prefix = '!', case_insensitive=True)
@@ -26,7 +27,7 @@ async def checkworking(ctx):
 async def covidcounter(ctx):
     URL = "https://www.worldometers.info/coronavirus/"
     r = requests.get(URL)
-    soup = BeautifulSoup(r.content, 'html5lib')
+    soup = BeautifulSoup(r.content, 'htmlparser')
     covidCases = soup.find('div', class_="maincounter-number")
     counter = covidCases.text.strip()
     print(counter)
@@ -36,7 +37,24 @@ async def covidcounter(ctx):
 @bot.slash_command(guild_ids=[879461344322138173], description="Information on COVID-19 Pandemic")
 async def covidinfo(ctx):
     await ctx.respond("For more information on COVID-19 visit: https://www.cdc.gov/coronavirus/2019-ncov/index.html")
+    
 
+@bot.slash_command(guild_ids=[879461344322138173], description="Request free at-home COVID tests")
+async def covidtest(ctx):
+    await ctx.respond("Request your free at-home COVID tests from the USPS: https://special.usps.com/testkits")
+    
+'''    
+@bot.slash_command(guild_ids=[879461344322138173], description="Check for vaccine locations near you")
+async def covidvaccine(ctx, 
+    zipcode:Option(int, max=5)
+    ):
+    search = SearchEngine()
+    zipcodeSearch = search.by_zipcode(zipcode)
+    zipList = zipcodeSearch.values()
+    majorCity = zipList[3]
+    await ctx.respond("I've got your city as %s" % majorCity)
+    
+'''
 
 
     
